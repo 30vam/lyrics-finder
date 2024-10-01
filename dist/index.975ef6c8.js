5d2b1d9fea779e4e846160073a4d1f4e7fa3d1e1
30vam <khodaparast2011@gmail.com>
2024-10-01 18:39:24 +0330

    Styled search results more

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -603,31 +603,35 @@ const displaySearchResults = (searchResults)=>{
     for (const hit of hits){
         const id = hit.result.id;
         const type = hit.type;
-        const title = hit.result.title;
+        const title = hit.result.title_with_featured;
         const artistName = hit.result.artist_names;
         const songImageThumbnailUrl = hit.result.header_image_thumbnail_url;
         const songImageUrl = hit.result.header_image_url;
         const artistHeaderImageUrl = hit.result.primary_artist.header_image_url;
         const artistImageUrl = hit.result.primary_artist.header_image_url;
-        const releaseDate = hit.result.release_date_for_display;
+        const releaseDate = hit.result.release_date_with_abbreviated_month_for_display;
         const newItem = document.createElement("div");
-        newItem.classList.add("rounded-2xl", "absolute", "bg-opacity-30", "backdrop-blur-xl", "hover:bg-opacity-60", "overflow-clip", "transition-all", "w-1/3");
+        newItem.classList.add("rounded-2xl", "relative", "bg-opacity-30", "backdrop-blur-xl", "hover:bg-opacity-60", "overflow-clip", "transition-all", "w-[40%]");
         const itemThumbnail = document.createElement("img");
         itemThumbnail.classList.add("w-full", "hover:scale-125", "transition-all");
         itemThumbnail.setAttribute("src", songImageThumbnailUrl);
         const songInfo = document.createElement("div");
-        songInfo.classList.add("bg-white", "bg-opacity-50", "backdrop-blur-lg", "hover:text-orange-500", "transition-all", "w-full", "h-[96px]", "text-center", "relative", "top-[50%]");
-        songInfo.innerText = `${title}\n${artistName} - ${releaseDate}`;
-        infoWrapper.append(newItem);
+        songInfo.classList.add("backdrop-blur-md", "hover:text-orange-500", "transition-all", "w-full", "h-fit", "py-2", "text-center", "absolute", "bottom-0");
+        songInfo.innerText = `${artistName} - ${releaseDate}`;
+        const songTitleDiv = document.createElement("div");
+        songTitleDiv.classList.add("font-bold", "text-lg", "mb-2");
+        songTitleDiv.innerText = title;
+        songInfo.prepend(songTitleDiv);
         newItem.append(itemThumbnail);
         newItem.append(songInfo);
+        infoWrapper.append(newItem);
     }
 };
 const handleSearchRequest = async (event)=>{
     console.log("Handling search query...");
     event.preventDefault();
     // We need to use await here, otherwise it will return the async FUNCTION instead of data, async functions ALWAYS return promise without waiting, so we need to use await
-    const searchResults = await (0, _searchSongsDefault.default)("metallica", accessToken);
+    const searchResults = await (0, _searchSongsDefault.default)("taylor swift", accessToken);
     displaySearchResults(searchResults);
 //getLyrics('The Unforgive II');
 };
